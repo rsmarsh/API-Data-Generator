589e5d2c0e81a810ef16adf104c76906ca338c8e
Richard Marshall <richard.marshall@customerfocus.com>
2020-04-03 16:01:35 +0100

    added informative root page for the api, switched to api path

--- index.js
+++ index.js
@@ -15,14 +15,30 @@ const APIList = {
 
 app.use(cors());
 
+const getEndpointList = () => {
+    let returnValue = ''
+    Object.keys(APIList).forEach((prop) => {returnValue+='/'+prop+'\n\t'});
+    return returnValue;
+};
+
 const server = app.listen(process.env.PORT || 3001, () => {
     console.log(`CORS-enabled server listening on ${server.address().port}`);
     console.log('There are '+Object.keys(APIList).length+' endpoints available. They are:');
-    Object.keys(APIList).forEach((prop) => {console.log(prop)});
+    console.log(getEndpointList());
+    
 });
 
+app.get('/', (req, res) => {
+    res.write(`
+    The following endpoints are active, by visiting /api:
+        ${getEndpointList()}
+    These endpoints have been hit ${endpointHits} times since the last deploy
+    `);
+    
+    res.end();
+});
 
-app.get('/endpoints/:endpointName', cors(), (req, res) => {
+app.get('/api/:endpointName', cors(), (req, res) => {
     console.log(`endpoint requested. ${endpointHits+=1} total hits.`);
     if (req.params && req.params.endpointName) {
 
